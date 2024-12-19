39b6d66070b9d85e5a4b8a11146bc1d589b63502
pinje <melvinaustinn@gmail.com>
2024-12-19 14:46:22 +0700

    refactor loginPage to using LoginViews

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,20 +1,10 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import LoginViews from "@/views/Auth/Login";
 
 const LoginPage = () => {
-  const { push, query } = useRouter();
-  const handlerLogin = () => {
-    push("/product");
-  };
-
   return (
-    <div>
-      <h1>Login Page</h1>
-      <button onClick={() => handlerLogin()}>login</button>
-      <p>
-        Don't have an account? <Link href={"/auth/register"}>Register</Link>
-      </p>
-    </div>
+    <>
+      <LoginViews />
+    </>
   );
 };
 
